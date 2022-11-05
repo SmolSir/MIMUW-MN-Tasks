539e7cf591354e582fac237fd21bdf156340772f
Bartosz Smolarczyk <bs429594@students.mimuw.edu.pl>
2022-11-05 21:13:50 +0100

    Implemented dTridiag.dot, added pretty print.

--- Bartosz_Smolarczyk-dTridiag.py
+++ Bartosz_Smolarczyk-dTridiag.py
@@ -3,24 +3,29 @@ import math
 
 class dTridiag:
     def __str__(self):
-        return f"n: {self.n}\n \
-                 d: {self.d}\n \
-                 diagonal:      {np.around(self.diagonal, decimals = 2)}\n \
-                 hyperdiagonal: {np.around(self.hyperdiagonal, decimals = 2)}\n \
-                 subdiagonal:   {np.around(self.subdiagonal, decimals = 2)}\n"
+        return f"dTridiag:\n \
+            n:             {self.n}\n \
+            d:             {self.d}\n \
+            diagonal:      {np.around(self.diagonal, decimals = 2)}\n \
+            hyperdiagonal: {np.around(self.hyperdiagonal, decimals = 2)}\n \
+            subdiagonal:   {np.around(self.subdiagonal, decimals = 2)}\n"
 
     def __init__(self, a:np.ndarray, b:np.ndarray, c:np.ndarray):
-        self.n = a.size
-        self.d = self.n - b.size
         self.diagonal = a
-        self.hyperdiagonal = np.concatenate([b, np.zeros(self.d)])
-        self.subdiagonal = np.concatenate([np.zeros(self.d), c])
+        self.hyperdiagonal = b
+        self.subdiagonal = c
+        self.n = self.diagonal.size
+        self.d = self.diagonal.size - self.hyperdiagonal.size
 
     def dot(self, v: np.ndarray):
-        pass
+        diag_comp = self.diagonal * v
+        hyperdiag_comp = np.concatenate((self.hyperdiagonal * v[self.d : ], np.zeros(self.d)))
+        subdiag_comp = np.concatenate((np.zeros(self.d), self.subdiagonal * v[ : self.d]))
+        return diag_comp + hyperdiag_comp + subdiag_comp
 
     def solve(self, y: np.ndarray):
         pass
 
 test_dTridiag = dTridiag(np.array([1, 3, 6, 7]), np.array([2, 4]), np.array([5, math.pi]))
 print(test_dTridiag)
+print(test_dTridiag.dot(np.array([1, 2, 3, 4])))
